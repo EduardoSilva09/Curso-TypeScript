530b9be9e5098d6de4faae796f961c39c5e1d6ae
Eduardo Silva Feitosa <88010447+EduardoSilva09@users.noreply.github.com>
2022-05-03 19:39:34 -0300

    correção variavel com o tipo any sendo atribuido implicitamente

--- src/aula002/tipo-any.ts
+++ src/aula002/tipo-any.ts
@@ -1,7 +1,7 @@
 //Definido como - msg: any
-function showMessage(msg) {
+function showMessage(msg: string) {
   return msg;
 }
 
 console.log(showMessage('tersts'));
-console.log(showMessage([1, 2, 3]));
+// console.log(showMessage([1, 2, 3]));
