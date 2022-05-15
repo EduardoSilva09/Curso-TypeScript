b05296221b171fb1b52611cf2829f6b2817f4fde
Eduardo Silva Feitosa <88010447+EduardoSilva09@users.noreply.github.com>
2022-05-15 17:32:58 -0300

    Aula 13 - Type Alias

---
+++ src/aula013/type-alias.ts
@@ -0,0 +1,27 @@
+// Por convenção os tipos criados são iniciados com letras maiusculas
+type Idade = number;
+type Pessoa = {
+  nome: string;
+  idade: Idade;
+  salario: number;
+  corPreferida?: CorPreferida;
+};
+
+type CorRGB = 'Vermelho' | 'Verde' | 'Azul';
+type CorCMYK = 'Ciano' | 'Magenta' | 'Amarelo' | 'Preto';
+type CorPreferida = CorRGB | CorCMYK;
+
+const pessoa: Pessoa = {
+  nome: 'Eduardo',
+  idade: 20,
+  salario: 200_000,
+  corPreferida: 'Ciano',
+};
+// console.log(pessoa);
+
+export function setCorPreferida(pessoa: Pessoa, cor: CorPreferida): Pessoa {
+  return { ...pessoa, corPreferida: cor };
+}
+
+console.log(setCorPreferida(pessoa, 'Preto'));
+console.log(pessoa); // Não altera o objeto original
