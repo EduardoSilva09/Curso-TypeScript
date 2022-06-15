77f560b9f0aa52dc68e79603362e04a8a50aa456
Eduardo Silva Feitosa <88010447+EduardoSilva09@users.noreply.github.com>
2022-06-15 16:03:31 -0300

    separada configuração de frontend e backend

---
+++ frontend/assets/js/bundle.js
@@ -0,0 +1,13 @@
+/******/ (() => {
+  // webpackBootstrap
+  /******/ 'use strict';
+  var __webpack_exports__ = {};
+  /*!*************************************!*\
+  !*** ./src/Aula18-webcack/index.ts ***!
+  \*************************************/
+
+  console.log('Olá mundo!');
+
+  /******/
+})();
+//# sourceMappingURL=bundle.js.map
